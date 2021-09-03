6a6e88c00bbc4aa3e31c23817393ace56f37de30
central.dogma99 <central.dogma99@gmail.com>
2021-09-04 02:48:31 +0900

    fix: updateKoreanBot

--- tools/koreanbot/updateKoreanBot.js
+++ tools/koreanbot/updateKoreanBot.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 const koreanBotToken = process.env.KOREANBOT_SECRET;
 
-function updateKoreanBot(){
-  axios.post(`https://koreanbots.dev/api/v2/bots/868188628709425162/stats`,
+function updateKoreanBot(num){
+  return () => axios.post(`https://koreanbots.dev/api/v2/bots/868188628709425162/stats`,
   {
-    "servers": client.guilds.cache.size
+    "servers": num
   },
   {
     headers: {
