7631fe74b581f506fafddf88a30c2f52aaa3e03c
central.dogma99 <central.dogma99@gmail.com>
2021-09-23 06:00:08 +0900

    fix: some tooltips

--- commands/leaderboard.ts
+++ commands/leaderboard.ts
@@ -9,7 +9,7 @@ async function leaderboard(message){
   let embed = new MessageEmbed()
     .setColor('#0099ff')
     .setTitle(`**여관주인 리더보드(KR)**`)
-    .setDescription('퀴즈를 풀거나 아래 링크에서 하트를 눌러 기여도를 획득할 수 있어요.\n[🔗 한국 디스코드봇 리스트!](https://koreanbots.dev/bots/868188628709425162)');
+    .setDescription('퀴즈를 풀거나 아래 링크에서 하트를 눌러 기여도를 획득할 수 있어요.\n배틀태그가 표시되지 않는다면 `@여관주인 !나`를 사용한 후에 다시 시도해 주세요.\n[🔗 한국 디스코드봇 리스트!](https://koreanbots.dev/bots/868188628709425162)');
   
   let i = 0;
   let str = "";
